cafe2ba39e35f7994c92e874eb428201dc50ebea
chen3t6y <chen3t6y@outlook.com>
2026-05-11 08:12:19 +0800

    修复: 如果在中文输入法下从插入模式切换到普通模式再到命令模式, 在返回插入模式时不会自动切换回中文输入法的 Bug

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -10,6 +10,20 @@ return {
       -- 指定使用 fcitx5-remote
       default_command = "fcitx5-remote",
 
+      -- Restore the default input method state when the following events are triggered
+      -- "VimEnter" and "FocusGained" were removed for causing problems, add it by your needs
+      -- set_default_events = { "InsertLeave", "CmdlineLeave" },
+      set_default_events = { "InsertLeave" },
+
+      -- Restore the previous used input method state when the following events
+      -- are triggered, if you don't want to restore previous used im in Insert mode,
+      -- e.g. deprecated `disable_auto_restore = 1`, just let it empty
+      -- as `set_previous_events = {}`
+      set_previous_events = { "InsertEnter" },
+
+      -- Show notification about how to install executable binary when binary missed
+      keep_quiet_on_no_binary = false,
+
       -- 开启异步切换，防止输入法切换时导致 Neovim 卡顿
       async_switch_im = true,
     })
